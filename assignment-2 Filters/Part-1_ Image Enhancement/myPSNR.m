afbe20a2ea0273be97aa338874daa3e197eee7fc
Ruben Laurentiu Grab <ruben.grab@gmail.com>
2018-02-26 17:15:53 +0100

    Implemented myPSnR and.. I can't remember what I changed in gauss1D

--- assignment-2 Filters/Part-1_ Image Enhancement/myPSNR.m
+++ assignment-2 Filters/Part-1_ Image Enhancement/myPSNR.m
@@ -1,5 +1,10 @@
 function [ PSNR ] = myPSNR( orig_image, approx_image )
-fprintf('Not implemented\n')
-
-end
-
+    [h,w, ~] = size(orig_image);
+    
+    orig_image = im2double(orig_image);
+    approx_image = im2double(approx_image);
+  
+    MSE = sum(sum((orig_image-approx_image).^2))/(h*w);
+    I_max = max(max(orig_image));
+    PSNR = 20*log10(double(I_max/sqrt(MSE)));
+end 
