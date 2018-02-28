106a139b100e279c49e2a54efb2ea2dc53597362
Ruben Laurentiu Grab <ruben.grab@gmail.com>
2018-02-28 22:33:38 +0100

    my last modifications

--- assignment-2 Filters/Part-1_ Image Enhancement/compute_LoG.m
+++ assignment-2 Filters/Part-1_ Image Enhancement/compute_LoG.m
@@ -1,25 +1,28 @@
 function imOut = compute_LoG(image, LOG_type)
 
-switch LOG_type
-    case 1
-        %method 1
-        gaussKernel = gauss2D(0.5,5);
-        laplacianKernel = fspecial('laplacian', 0.2);
-        smoothedImage = imfilter(im2double(image), gaussKernel);
-        imOut = imfilter(smoothedImage, laplacianKernel);
-    case 2
-        %method 2
-        logKernel = fspecial('log', 5, 0.5);
-        imOut = imfilter(im2double(image), logKernel);
-    case 3
-        gaussKernelHigh = gauss2D(sqrt(2) * 0.5, 5);
-        imFilterHigh = imfilter(im2double(image), gaussKernelHigh);
+    switch LOG_type
+        case 1
+            %method 1
+            gaussKernel = gauss2D(0.5,5);
+            laplacianKernel = fspecial('laplacian', 0.2);
+            smoothedImage = imfilter(image, gaussKernel);
+            imOut = imfilter(smoothedImage, laplacianKernel);
+        case 2
+            %method 2
+            logKernel = fspecial('log', 5, 0.5);
+            imOut = imfilter(image, logKernel);
+        case 3
+            gaussKernelHigh = gauss2D(sqrt(2) * 0.5, 5);
+            imFilterHigh = imfilter(image, gaussKernelHigh);
+
+            gaussKernelLow = gauss2D(0.5, 5);
+            imFilterLow = imfilter(image, gaussKernelLow);
+            imOut = imFilterHigh - imFilterLow;
+
+            imOut  = mat2gray(imOut);
+    end
         
-        gaussKernelLow = gauss2D(0.5, 5);
-        imFilterLow = imfilter(im2double(image), gaussKernelLow);
-        imOut = imFilterHigh - imFilterLow;
-end
 figure
-imshow(imOut, [])
+imshow(imOut)
 end
 
--- assignment-2 Filters/Part-2_ Gabor Segmentation/gabor_segmentation.m
+++ assignment-2 Filters/Part-2_ Gabor Segmentation/gabor_segmentation.m
@@ -41,7 +41,7 @@ switch image_id
 end
 
 % Image adjustments
-img      = imresize(img,resize_factor);
+img      = im2double(imresize(img,resize_factor));
 img_gray = rgb2gray(img);
 
 % Display image
@@ -74,7 +74,7 @@ orientations = 0:dTheta:(pi/2);
 
 % Define the set of sigmas for the Gaussian envelope. Sigma here defines 
 % the standard deviation, or the spread of the Gaussian. 
-sigmas = [1,2];
+sigmas = [0.5,1,2,3];
 
 % Now you can create the filterbank. We provide you with a MATLAB struct
 % called gaborFilterBank in which we will hold the filters and their
@@ -155,105 +155,105 @@ for jj = 1 : length(gaborFilterBank)
     end
 end
 
-%% Question 5 Analysis
-
-[numRows, numCols, ~] = size(img);
-
-% Estimate the minimum and maximum of the wavelengths for the sinusoidal
-% carriers. 
-% ** This step is pretty much standard, therefore, you don't have to
-%    worry about it. It is cycles in pixels. **   
-lambdaMin = 4/sqrt(2);
-lambdaMax = hypot(numRows,numCols);
-
-% Specify the carrier wavelengths.  
-% (or the central frequency of the carrier signal, which is 1/lambda)
-n = floor(log2(lambdaMax/lambdaMin));
-lambdas = 2.^(0:(n-2)) * lambdaMin;
-
-lambdas = lambdas(1:2);
-
-% Define the set of orientations for the Gaussian envelope.
-dTheta      = pi/4;                  % \\ the step size LOL pi/4 is not good enough for you
-% orientations = -pi/2:dTheta:(pi/2);
-orientations = [pi/2];
-
-% Define the set of sigmas for the Gaussian envelope. Sigma here defines 
-% the standard deviation, or the spread of the Gaussian. 
-% sigmas = [0.5,1,2,3,4];
-sigmas = [1,2];
-
-
-gammas  = [0.25, 0.5, 0.75, 1];
-% Now you can create the filterbank. We provide you with a MATLAB struct
-% called gaborFilterBank in which we will hold the filters and their
-% corresponding parameters such as sigma, lambda and etc. 
-% ** All you need to do is to implement createGabor(). Rest will be handled
-%    by the provided code block. **
-gaborFilterBank = struct();
-tic
-filterNo = 1;
-for ii = 1:length(lambdas)
-    for jj  = 1:length(sigmas)
-        for kk = 1:length(gammas)
-            for ll = 1:length(orientations)
-                % Filter parameter configuration for this filter.
-                lambda = lambdas(ii);
-                sigma  = sigmas(jj);
-                gamma = gammas(kk);
-                theta  = orientations(ll);
-                psi    = 0;
-
-                % Create a Gabor filter with the specs above. 
-                % (We also record the settings in which they are created. )
-                % // TODO: Implement the function createGabor() following
-                %          the guidelines in the given function template.
-                %          ** See createGabor.m for instructions ** //
-                gaborFilterBank(filterNo).filterPairs = createGabor( sigma, theta, lambda, psi, gamma );
-                gaborFilterBank(filterNo).sigma       = sigma;
-                gaborFilterBank(filterNo).lambda      = lambda;
-                gaborFilterBank(filterNo).theta       = theta;
-                gaborFilterBank(filterNo).psi         = psi;
-                gaborFilterBank(filterNo).gamma       = gamma;
-                filterNo = filterNo+1;
-            end
-        end
-    end
-end
-ctime = toc; 
-
-fprintf('--------------------------------------\n \t\tDetails\n--------------------------------------\n')
-fprintf('Total number of filters       : %d \n', length(gaborFilterBank));
-fprintf('Number of scales (sigma)      : %d \n', length(sigmas));
-fprintf('Number of orientations (theta): %d \n', length(orientations));
-fprintf('Number of carriers (lambda)   : %d \n', length(lambdas));
-fprintf('--------------------------------------\n')
-fprintf('Filter bank created in %.3f seconds.\n', ctime);
-fprintf('--------------------------------------\n')
-
-featureMaps = cell(length(gaborFilterBank),1);
-for jj = 1 : length(gaborFilterBank)
-    real_out = imfilter(img_gray, gaborFilterBank(jj).filterPairs(:,:,1));
-    imag_out = imfilter(img_gray, gaborFilterBank(jj).filterPairs(:,:,2));
-    featureMaps{jj} = cat(3, real_out, imag_out);
-    
-    imwrite(real_out, sprintf('analysis/question_5/%s/filter_l%f_t%f_s%f_g%f_real.png', ...
-            image_id, gaborFilterBank(jj).lambda, gaborFilterBank(jj).theta, gaborFilterBank(jj).sigma, gaborFilterBank(jj).gamma))
-    imwrite(imag_out, sprintf('analysis/question_5/%s/filter_l%f_t%f_s%f_g%f_imag.png', ...
-            image_id, gaborFilterBank(jj).lambda, gaborFilterBank(jj).theta, gaborFilterBank(jj).sigma, gaborFilterBank(jj).gamma))
-    if visFlag
-        figure(2)
-        subplot(121), imshow(real_out), title(sprintf('Re[h(x,y)], \\lambda = %f, \\theta = %f, \\sigma = %f', ...
-            gaborFilterBank(jj).lambda,...
-            gaborFilterBank(jj).theta,...
-            gaborFilterBank(jj).sigma));
-        subplot(122), imshow(imag_out), title(sprintf('Im[h(x,y)], \\lambda = %f, \\theta = %f, \\sigma = %f', ...
-            gaborFilterBank(jj).lambda,...
-            gaborFilterBank(jj).theta,...
-            gaborFilterBank(jj).sigma));
-        pause(1)
-    end
-end
+% %% Question 5 Analysis
+% 
+% [numRows, numCols, ~] = size(img);
+% 
+% % Estimate the minimum and maximum of the wavelengths for the sinusoidal
+% % carriers. 
+% % ** This step is pretty much standard, therefore, you don't have to
+% %    worry about it. It is cycles in pixels. **   
+% lambdaMin = 4/sqrt(2);
+% lambdaMax = hypot(numRows,numCols);
+% 
+% % Specify the carrier wavelengths.  
+% % (or the central frequency of the carrier signal, which is 1/lambda)
+% n = floor(log2(lambdaMax/lambdaMin));
+% lambdas = 2.^(0:(n-2)) * lambdaMin;
+% 
+% lambdas = lambdas(1:2);
+% 
+% % Define the set of orientations for the Gaussian envelope.
+% dTheta      = pi/4;                  % \\ the step size LOL pi/4 is not good enough for you
+% % orientations = -pi/2:dTheta:(pi/2);
+% orientations = [pi/2];
+% 
+% % Define the set of sigmas for the Gaussian envelope. Sigma here defines 
+% % the standard deviation, or the spread of the Gaussian. 
+% % sigmas = [0.5,1,2,3,4];
+% sigmas = [1,2];
+% 
+% 
+% gammas  = [0.25, 0.5, 0.75, 1];
+% % Now you can create the filterbank. We provide you with a MATLAB struct
+% % called gaborFilterBank in which we will hold the filters and their
+% % corresponding parameters such as sigma, lambda and etc. 
+% % ** All you need to do is to implement createGabor(). Rest will be handled
+% %    by the provided code block. **
+% gaborFilterBank = struct();
+% tic
+% filterNo = 1;
+% for ii = 1:length(lambdas)
+%     for jj  = 1:length(sigmas)
+%         for kk = 1:length(gammas)
+%             for ll = 1:length(orientations)
+%                 % Filter parameter configuration for this filter.
+%                 lambda = lambdas(ii);
+%                 sigma  = sigmas(jj);
+%                 gamma = gammas(kk);
+%                 theta  = orientations(ll);
+%                 psi    = 0;
+% 
+%                 % Create a Gabor filter with the specs above. 
+%                 % (We also record the settings in which they are created. )
+%                 % // TODO: Implement the function createGabor() following
+%                 %          the guidelines in the given function template.
+%                 %          ** See createGabor.m for instructions ** //
+%                 gaborFilterBank(filterNo).filterPairs = createGabor( sigma, theta, lambda, psi, gamma );
+%                 gaborFilterBank(filterNo).sigma       = sigma;
+%                 gaborFilterBank(filterNo).lambda      = lambda;
+%                 gaborFilterBank(filterNo).theta       = theta;
+%                 gaborFilterBank(filterNo).psi         = psi;
+%                 gaborFilterBank(filterNo).gamma       = gamma;
+%                 filterNo = filterNo+1;
+%             end
+%         end
+%     end
+% end
+% ctime = toc; 
+% 
+% fprintf('--------------------------------------\n \t\tDetails\n--------------------------------------\n')
+% fprintf('Total number of filters       : %d \n', length(gaborFilterBank));
+% fprintf('Number of scales (sigma)      : %d \n', length(sigmas));
+% fprintf('Number of orientations (theta): %d \n', length(orientations));
+% fprintf('Number of carriers (lambda)   : %d \n', length(lambdas));
+% fprintf('--------------------------------------\n')
+% fprintf('Filter bank created in %.3f seconds.\n', ctime);
+% fprintf('--------------------------------------\n')
+% 
+% featureMaps = cell(length(gaborFilterBank),1);
+% for jj = 1 : length(gaborFilterBank)
+%     real_out = imfilter(img_gray, gaborFilterBank(jj).filterPairs(:,:,1));
+%     imag_out = imfilter(img_gray, gaborFilterBank(jj).filterPairs(:,:,2));
+%     featureMaps{jj} = cat(3, real_out, imag_out);
+%     
+%     imwrite(real_out, sprintf('analysis/question_5/%s/filter_l%f_t%f_s%f_g%f_real.png', ...
+%             image_id, gaborFilterBank(jj).lambda, gaborFilterBank(jj).theta, gaborFilterBank(jj).sigma, gaborFilterBank(jj).gamma))
+%     imwrite(imag_out, sprintf('analysis/question_5/%s/filter_l%f_t%f_s%f_g%f_imag.png', ...
+%             image_id, gaborFilterBank(jj).lambda, gaborFilterBank(jj).theta, gaborFilterBank(jj).sigma, gaborFilterBank(jj).gamma))
+%     if visFlag
+%         figure(2)
+%         subplot(121), imshow(real_out), title(sprintf('Re[h(x,y)], \\lambda = %f, \\theta = %f, \\sigma = %f', ...
+%             gaborFilterBank(jj).lambda,...
+%             gaborFilterBank(jj).theta,...
+%             gaborFilterBank(jj).sigma));
+%         subplot(122), imshow(imag_out), title(sprintf('Im[h(x,y)], \\lambda = %f, \\theta = %f, \\sigma = %f', ...
+%             gaborFilterBank(jj).lambda,...
+%             gaborFilterBank(jj).theta,...
+%             gaborFilterBank(jj).sigma));
+%         pause(1)
+%     end
+% end
 %% Compute the magnitude
 % Now, you will compute the magnitude of the output responses.
 % \\ Hint: (real_part^2 + imaginary_part^2)^(1/2) \\
@@ -261,7 +261,7 @@ featureMags =  cell(length(gaborFilterBank),1);
 for jj = 1:length(featureMaps)
     real_part = featureMaps{jj}(:,:,1);
     imag_part = featureMaps{jj}(:,:,2);
-    featureMags{jj} = (double(real_part) .^2 + double(imag_part) .^2).^(1/2);
+    featureMags{jj} = (real_part .^2 + imag_part .^2).^(1/2);
     
     % Visualize the magnitude response if you wish.
     if visFlag
@@ -292,6 +292,9 @@ if smoothingFlag
         % i)  filter the magnitude response with appropriate Gaussian kernels
         % ii) insert the smoothed image into features(:,:,jj)
     %END_FOR
+    for jj = 1:length(featureMags)
+        features(:,:,jj) = imgaussfilt(featureMags{jj},gaborFilterBank(jj).sigma*2);
+    end
 else
     % Don't smooth but just insert magnitude images into the matrix
     % called features.
@@ -311,9 +314,10 @@ features = reshape(features, numRows * numCols, []);
 % Standardize features. 
 % \\ Hint: see http://ufldl.stanford.edu/wiki/index.php/Data_Preprocessing
 %          for more information. \\
-
-features = 0; % \\ TODO: i)  Implement standardization on matrix called features. 
-             %          ii) Return the standardized data matrix.
+% \\ TODO: i)  Implement standardization on matrix called features. 
+%          ii) Return the standardized data matrix.
+features = (features - mean(features)) ./ std(features); 
+           
 
 
 % (Optional) Visualize the saliency map using the first principal component 
@@ -331,11 +335,9 @@ imshow(feature2DImage,[]), title('Pixel representation projected onto first PC')
 % \\ Hint-2: use the parameter k defined in the first section when calling
 %            MATLAB's built-in kmeans function.
 tic
-pixLabels = 0; % \\TODO: Return cluster labels per pixel
+pixLabels = kmeans(features, k); % \\TODO: Return cluster labels per pixel
 ctime = toc;
 fprintf('Clustering completed in %.3f seconds.\n', ctime);
-
-
 
 % Visualize the clustering by reshaping pixLabels into original grayscale
 % input size [numRows numCols].
