6f3e4580f030fcc589c0152485db6bd71137c700
andrei sili <andrei.sili1994@gmail.com>
2018-02-28 23:22:01 +0100

    assignment 2 done

--- assignment-2 Filters/Part-1_ Image Enhancement/compute_LoG.m
+++ assignment-2 Filters/Part-1_ Image Enhancement/compute_LoG.m
@@ -5,19 +5,22 @@ switch LOG_type
         %method 1
         gaussKernel = gauss2D(0.5,5);
         laplacianKernel = fspecial('laplacian', 0.2);
-        smoothedImage = imfilter(im2double(image), gaussKernel);
+        smoothedImage = imfilter(image, gaussKernel);
         imOut = imfilter(smoothedImage, laplacianKernel);
     case 2
         %method 2
         logKernel = fspecial('log', 5, 0.5);
-        imOut = imfilter(im2double(image), logKernel);
+        imOut = imfilter(image, logKernel);
     case 3
         gaussKernelHigh = gauss2D(sqrt(2) * 0.5, 5);
-        imFilterHigh = imfilter(im2double(image), gaussKernelHigh);
+        imFilterHigh = imfilter(image, gaussKernelHigh);
         
         gaussKernelLow = gauss2D(0.5, 5);
-        imFilterLow = imfilter(im2double(image), gaussKernelLow);
+        imFilterLow = imfilter(image, gaussKernelLow);
+        
         imOut = imFilterHigh - imFilterLow;
+        imOut  = mat2gray(imOut);
+        
 end
 figure
 imshow(imOut, [])
