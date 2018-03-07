979cc868ec052cb9f87cfe0468d9e6af17f7f7f6
andrei sili <andrei.sili1994@gmail.com>
2018-03-07 22:08:26 +0100

    finished harris corner detector

--- assignment-3 Corner Detection/harris_corner_detector.m
+++ assignment-3 Corner Detection/harris_corner_detector.m
@@ -1,7 +1,20 @@
-function [H, r, c] = harris_corner_detector(img_path, sigma, threshold)
+function [H, r, c] = harris_corner_detector(img_path, sigma, threshold, show_gauss, show_derivative_corner, show_only_corner)
+
+switch nargin
+    case 3
+        show_gauss = false;
+        show_derivative_corner = false;
+        show_only_corner = false;
+    case 4
+        show_derivative_corner = false;
+        show_only_corner = false;
+    case 5
+        show_only_corner = false;
+end
 
 % Reading image and setting window size
-I = double(rgb2gray(imread(img_path)));
+colorI = imread(img_path);
+I = double(rgb2gray(colorI));
 n = 6 * sigma;
 
 % Generating the derivative of Gasussian
@@ -24,27 +37,40 @@ windowMax = ordfilt2(H, n^2, true(n));
 maxLocations = (H == windowMax) & (H > threshold);
 [r,c] = find(maxLocations);
 
-figure
-subplot(1,2,1);
-surf(1:length(Gx), 1:length(Gy), Gx)
-title('Gaussian derivative horizontal direction');
-subplot(1,2,2)
-surf(1:length(Gx), 1:length(Gy), Gy)
-title('Gaussian derivative vertical direction');
+if show_gauss
+    figure
+    subplot(1,2,1);
+    surf(1:length(Gx), 1:length(Gy), Gx)
+    title('Gaussian derivative horizontal direction');
+    subplot(1,2,2)
+    surf(1:length(Gx), 1:length(Gy), Gy)
+    title('Gaussian derivative vertical direction');
+end
 
-% Plots
-figure
-subplot(1,3,1)
-title('Derivatives and corners');
-imshow(Ix, [])
-title('Derivative in horiontal direction')
-subplot(1,3,2)
-imshow(Iy, [])
-title('Derivative in vertical direction')
-subplot(1,3,3)
-imshow(I, [])
-hold on
-plot(c(:), r(:), 'r+')
-title('Corners')
+if show_derivative_corner
+    figure
+    subplot('Position', [0 0.1 0.32 0.32])
+    imshow(Ix, [])
+    title('Derivative in horizontal direction');
+    subplot('Position', [0.33 0.1 0.32 0.32]);
+    imshow(Iy, [])
+    title('Derivative in vertical direction')
+    subplot('Position', [0.66 0.1 0.32 0.32]);
+    imshow(colorI, [])
+    hold on
+    plot(c(:), r(:), 'r+')
+    title('Corners');
+    print(sprintf('corner_detection_s%.3f_t%.3f.png', sigma, threshold),'-dpng')
+end
+
+if show_only_corner
+    figure
+    imshow(colorI, [])
+    hold on
+    plot(c(:), r(:), 'r+')
+    a = strsplit(img_path, '/');
+    b = strcat('rotation_', a(2));
+    print(sprintf('%s', b{1}),'-dpng');
+end
 
 end
