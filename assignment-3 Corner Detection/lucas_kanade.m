16f0b4c1c6cfa411d8ae3a74d0fe84b7e6a306a1
Ruben Laurentiu Grab <ruben.grab@gmail.com>
2018-03-07 21:29:26 +0100

    lucas-kanade

--- assignment-3 Corner Detection/lucas_kanade.m
+++ assignment-3 Corner Detection/lucas_kanade.m
@@ -1,11 +1,17 @@
 function lucas_kanade(img1, img2)
+    window_size = 15;
+    
+    if mod(window_size,2) == 0
+        fprintf("Window size must be odd.");
+    end
+
     [h, w, n] = size(img1);
     if n == 3
         img1_gray = im2double(rgb2gray(img1));
         img2_gray = im2double(rgb2gray(img2));
     else
-        img1_gray = img1;
-        img2_gray = img2;
+        img1_gray = im2double(img1);
+        img2_gray = im2double(img2);
     end
 
     xs = [];
@@ -16,12 +22,12 @@ function lucas_kanade(img1, img2)
     Ix_m = conv2(img1_gray,[-1 1; -1 1], 'valid'); % partial on x
     Iy_m = conv2(img1_gray, [-1 -1; 1 1], 'valid'); % partial on y
     It_m = conv2(img1_gray, ones(2), 'valid') + conv2(img2_gray, -ones(2), 'valid'); % partial on t
-    for i= 1:15:(h-15)
-        for j= 1:15:(w-15)
+    for i= 1:window_size:(h-window_size)
+        for j= 1:window_size:(w-window_size)
             
-            Ix = Ix_m(i:i+15-1,j:j+15-1,:);
-            Iy = Iy_m(i:i+15-1,j:j+15-1,:);
-            It = It_m(i:i+15-1,j:j+15-1,:);
+            Ix = Ix_m(i:i+window_size-1,j:j+window_size-1,:);
+            Iy = Iy_m(i:i+window_size-1,j:j+window_size-1,:);
+            It = It_m(i:i+window_size-1,j:j+window_size-1,:);
             
             A = [Ix(:), Iy(:)];
             A_t = A';
@@ -29,8 +35,8 @@ function lucas_kanade(img1, img2)
             
             v = (A_t * A) \ (A_t * b);
             
-            xs = [xs,j+7];
-            ys = [ys,i+7];            
+            xs = [xs,j+floor(window_size/2)];
+            ys = [ys,i+floor(window_size/2)];            
             us = [us,v(1)];
             vs = [vs,v(2)];         
         end
