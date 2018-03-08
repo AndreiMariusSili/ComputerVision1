e31bd62b0d75249def3cdb23accb9184899d27f7
Ruben Laurentiu Grab <ruben.grab@gmail.com>
2018-03-08 18:33:59 +0100

    nada

---
+++ assignment-4 Image Alignment and Stitching/image_alignment.m
@@ -0,0 +1,7 @@
+boat1 = single(imread('./boat1.pgm')); 
+[f1,d1] = vl_sift(boat1) ;
+
+boat2 = single(imread('./boat2.pgm')); 
+[f2,d2] = vl_sift(boat2) ;
+
+[matches, scores] = vl_ubcmatch(da, db) ;
