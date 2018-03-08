58d3e52dc74b5dcd33f8d3819be7bb11ca243ff0
andrei sili <andrei.sili1994@gmail.com>
2018-03-08 14:26:47 +0100

    feature tracking

--- assignment-3 Corner Detection/lucas_kanade.m
+++ assignment-3 Corner Detection/lucas_kanade.m
@@ -1,51 +1,82 @@
-function [xs,ys,us,vs] = lucas_kanade(img1, img2, window_size, step_size)
-    if mod(window_size,2) == 0
-        fprintf("Window size must be odd!");
+function [xs,ys,us,vs] = lucas_kanade(img1, img2, xy)
+    window = 15;
+    half = floor(window/2);
+    [h, w, n] = size(img1);
+    if n == 3
+        img1_gray = im2double(rgb2gray(img1));
+        img2_gray = im2double(rgb2gray(img2));
     else
-        half = floor(window_size/2);
-        [h, w, n] = size(img1);
-        if n == 3
-            img1_gray = im2double(rgb2gray(img1));
-            img2_gray = im2double(rgb2gray(img2));
-        else
-            img1_gray = im2double(img1);
-            img2_gray = im2double(img2);
+        img1_gray = im2double(img1);
+        img2_gray = im2double(img2);
+    end
+    
+    switch nargin
+        case 2
+            step_size = 15; 
+            x_range = 1+half:step_size:(h-half);
+            y_range = 1+half:step_size:(w-half);
+            [X,Y] = meshgrid(x_range, y_range);
+            X = X(:);
+            Y = Y(:);
+        case 3
+            x_range = xy(:, 1);
+            X = x_range(:);
+            y_range = xy(:, 2);
+            Y = y_range(:);
+    end
+
+    xs = [];
+    ys = [];
+    us = [];
+    vs = [];
+
+    % Generating the derivatives
+    Ix_m = conv2(img1_gray,[-1 1; -1 1], 'same'); % partial on x
+    Iy_m = conv2(img1_gray, [-1 -1; 1 1], 'same'); % partial on y
+    It_m = conv2(img1_gray, ones(2), 'same') + conv2(img2_gray, -ones(2), 'same'); % partial on t
+
+    
+    for ii = 1:length(X)
+        i = X(ii);
+        j = Y(ii);
+        
+        if (i-half < 1) || (i+half > h+1) || (j-half < 1) || (j+half > w+1)
+            xs = [xs;j];
+            ys = [ys;i];
+            us = [us;0];
+            vs = [vs;0];
+            continue;
         end
 
-        xs = [];
-        ys = [];
-        us = [];
-        vs = [];
+        Ix = Ix_m(i-half:i+half,j-half:j+half,:);
+        Iy = Iy_m(i-half:i+half,j-half:j+half,:);
+        It = It_m(i-half:i+half,j-half:j+half,:);
 
-        Ix_m = conv2(img1_gray,[-1 1; -1 1], 'valid'); % partial on x
-        Iy_m = conv2(img1_gray, [-1 -1; 1 1], 'valid'); % partial on y
-        It_m = conv2(img1_gray, ones(2), 'valid') + conv2(img2_gray, -ones(2), 'valid'); % partial on t
-        for i= 1+half:step_size:(h-half)
-            for j= 1+half:step_size:(w-half)
+        A = [Ix(:), Iy(:)];
+        b = -It(:);
+    
+        v = pinv(A)*b;        
+        
+        xs = [xs;j];
+        ys = [ys;i];
+        us = [us;v(1)];
+        vs = [vs;v(2)];
+    end
 
-                Ix = Ix_m(i-half:i+half,j-half:j+half,:);
-                Iy = Iy_m(i-half:i+half,j-half:j+half,:);
-                It = It_m(i-half:i+half,j-half:j+half,:);
-
-                A = [Ix(:), Iy(:)];
-                A_t = A';
-                b = -It(:);
-
-                v = (A_t * A) \ (A_t * b);
-
-                xs = [xs,j];
-                ys = [ys,i];            
-                us = [us,v(1)];
-                vs = [vs,v(2)];         
-            end
-        end
-        us(isnan(us))=0;
-        vs(isnan(vs))=0;
-
-        figure
-        imshow(img1);
-        hold on
-        quiver(xs,ys,us,vs, 'r');
-        hold off 
+    switch nargin
+        case 2
+            figure;
+            imshow(img1)
+            hold on
+            quiver(xs,ys,us,vs, 'r');
+            hold off
+        case 3
+            figure;
+            set(gcf, 'Visible', 'off');
+            imshow(img1_gray, [])
+            hold on
+            quiver(xs,ys,us,vs, 'r');
+            plot(xs, ys, 'r.');
+            hold off
     end
 end
---
+++ assignment-3 Corner Detection/tracking.m
@@ -0,0 +1,35 @@
+clear; clc;
+folder = 'pingpong/';
+file_pattern = fullfile(folder, '*.jpeg');
+files = dir(file_pattern);
+
+first_frame_path = fullfile(folder, files(1).name);
+resized_first_frame_path = fullfile('resized/', 'pingpong.jpeg');
+imwrite(imresize(imread(first_frame_path), [200, NaN]), 'resized/pingpong.jpeg');
+
+result_path = 'feature_tracking_15_resized_pingpong.gif';
+
+[H,r,c] = harris_corner_detector(resized_first_frame_path, 1.5, 1000,false,false,false);
+r = r(:);
+c = c(:);
+
+for k = 1:length(files)-1
+    current_frame = imread(fullfile(folder, files(k).name));
+    current_frame = imresize(current_frame, [200, NaN]);
+    next_frame = imread(fullfile(folder, files(k+1).name));
+    next_frame = imresize(next_frame, [200, NaN]);
+    [xs,ys,us,vs] = lucas_kanade(current_frame, next_frame, [r, c]);
+    
+    frame = getframe;
+    im = frame2im(frame);
+    [imind,cm] = rgb2ind(im,256);
+    if k == 1
+      imwrite(imind,cm,result_path,'gif', 'Loopcount',inf);
+    else 
+      imwrite(imind,cm,result_path,'gif','WriteMode','append');
+    end
+    
+    r = round(r + vs);
+    c = round(c + us);
+end
+disp('done');
